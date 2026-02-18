8b9c00b9b04ac5814f1081538c6dd626300d6768
AlexClaridge <alexjclaridge@gmail.com>
2026-02-18 18:01:02 +0000

    Messed around with shaders when you toggle lucid

---
+++ BrackeysGameJam/Assets/Rendering/ToggleShaders.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ToggleShaders : MonoBehaviour
+{
+    [SerializeField] Volume volume;
+    Vignette vignette;
+    ChromaticAberration chromaticAberration;
+    WhiteBalance whiteBalance;
+    MotionBlur motiionBlur;
+    DepthOfField depthOfField;
+
+    bool setupProperly = true;
+
+    private void Awake()
+    {
+        if (!volume.profile.TryGet<Vignette>(out vignette))
+            setupProperly = false;
+        if (!volume.profile.TryGet<ChromaticAberration>(out chromaticAberration))
+            setupProperly = false;
+        if (!volume.profile.TryGet<WhiteBalance>(out whiteBalance))
+            setupProperly = false;
+        if (!volume.profile.TryGet<MotionBlur>(out motiionBlur))
+            setupProperly = false;
+        if (!volume.profile.TryGet<DepthOfField>(out depthOfField))
+            setupProperly = false;
+    }
+
+    private void OnEnable()
+    {
+        AsleepLucidControl.onLucidToggled += ToggleShadersFromLucid;
+    }
+
+    private void OnDisable()
+    {
+        AsleepLucidControl.onLucidToggled -= ToggleShadersFromLucid;    
+    }
+
+    private void ToggleShadersFromLucid(bool isLucid)
+    {
+        if (!setupProperly)
+        {
+            Debug.LogWarning("Not setup volume for shader toggle on lucid");
+            return;
+        }
+        vignette.active = !isLucid;
+        chromaticAberration.active = isLucid;
+        whiteBalance.active = isLucid;
+        motiionBlur.active = isLucid;
+        depthOfField.active = isLucid;
+    }
+}
