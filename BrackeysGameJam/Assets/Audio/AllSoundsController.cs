7b41edabac03bd52b6638f007d227c8f97497557
Alex Claridge <alexjclaridge@gmail.com>
2026-02-19 14:34:16 +0000

    I have done commit

--- BrackeysGameJam/Assets/Audio/AllSoundsController.cs
+++ BrackeysGameJam/Assets/Audio/AllSoundsController.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AllSoundsController : MonoBehaviour
 {
     const float musicStartTimeMin = 0.5f;
     const float musicStartTimeMax = 50f;
-    
+
+    [SerializeField] float footStepFrequencyBedroom;
+    [SerializeField] float footStepFrequencyDream;
+    [SerializeField] float randomWhisperFrequencyBedroom;
+    [SerializeField] float randomWhisperFrequencyDream;
     [SerializeField][Range(musicStartTimeMin, musicStartTimeMax)] float musicStartTimeMaxForRandom = 0.5f;
     public SceneName currentScene;
+    bool walking = false;
+    float timeWalking = 0;
+    float timeWhispers = 0;
 
     private void OnEnable()
     {
         TransitionManager.onLoadingNextScene += NewScene;
+        //+= StartWalking;
+        //+= StopWalking;
     }
 
     private void OnDisable()
     {
         TransitionManager.onLoadingNextScene -= NewScene;
+        //+= StartWalking;
+        //+= StopWalking;
     }
 
     private void Start()
@@ -46,6 +58,49 @@ public class AllSoundsController : MonoBehaviour
 
         // Kick off ambience
         PlayAmbience();
+    }
+
+    private void Update()
+    {
+        // If we're in any of the current scenes
+        switch (currentScene)
+        {
+            case SceneName.AWAKEPARALYZED1:
+            case SceneName.AWAKEPARALYZED2:
+            case SceneName.AWAKEPARALYZED3:
+            case SceneName.AWAKEPARALYZED4:
+            case SceneName.AWAKEPARALYZED5:
+            case SceneName.MAZE1:
+            case SceneName.MAZE2:
+            case SceneName.MAZE3:
+            case SceneName.MAZE4:
+            case SceneName.MAZE5:
+                // Randomly Play Whispers
+                timeWhispers += Time.deltaTime;
+                PlayRandomWhisper();
+                break;
+
+            default:
+                // Don't try to play whispers
+                break;
+        }
+
+        timeWhispers += Time.deltaTime;
+        if (currentScene == SceneName.AWAKEBEGINNING)
+        {
+            if (timeWalking > footStepFrequencyBedroom)
+            {
+                timeWalking = 0;
+                //AudioManager.instance.PlaySFX("")
+            }
+        }
+        else
+        {
+            if (timeWalking > footStepFrequencyDream)
+            {
+                timeWalking = 0;
+            }
+        }
     }
 
     void NewScene(SceneName name)
@@ -82,5 +137,87 @@ public class AllSoundsController : MonoBehaviour
         //} TODO: this
 
 
+    }
+
+    void StartWalking()
+    {
+        walking = true;
+
+        if (currentScene == SceneName.AWAKEBEGINNING)
+        {
+            if (timeWalking > footStepFrequencyBedroom / 2)
+            {
+                //AudioManager.instance.PlaySFX("")
+            }
+        }
+        else
+        {
+            if (timeWalking > footStepFrequencyDream / 2)
+            {
+                //AudioManager.instance.PlaySFX("")
+            }
+        }
+    }
+
+    void StopWalking()
+    {
+        walking = false;
+    }
+
+    IEnumerator Walking()
+    {
+        while (walking)
+        {
+            timeWalking += Time.deltaTime;
+            if (currentScene == SceneName.AWAKEBEGINNING)
+            {
+                if (timeWalking > footStepFrequencyBedroom)
+                {
+                    timeWalking = 0;
+                    //AudioManager.instance.PlaySFX("")
+                }
+            }
+            else
+            {
+                if (timeWalking > footStepFrequencyDream)
+                {
+                    timeWalking = 0;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    void PlayRandomWhisper()
+    {
+        bool playWhisper = false;
+        switch (currentScene)
+        {
+            case SceneName.AWAKEPARALYZED1:
+            case SceneName.AWAKEPARALYZED2:
+            case SceneName.AWAKEPARALYZED3:
+            case SceneName.AWAKEPARALYZED4:
+            case SceneName.AWAKEPARALYZED5:
+                playWhisper = (Random.Range(0, randomWhisperFrequencyBedroom) < timeWhispers);
+                break;
+            case SceneName.MAZE1:
+            case SceneName.MAZE2:
+            case SceneName.MAZE3:
+            case SceneName.MAZE4:
+            case SceneName.MAZE5:
+                playWhisper = (Random.Range(0, randomWhisperFrequencyDream) < timeWhispers);
+                break;
+            default:
+                Debug.LogWarning("Errror, shouldn't be able to get here!");
+                break;
+        }
+
+        // If we want to play a whisper
+        if (playWhisper)
+        {
+
+        }
+        // pick whisper to play
     }
 }
